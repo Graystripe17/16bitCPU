dde6d076a07e5a1105b7e3bd117f0630f232985a
Winston Van <winston.van7@gmail.com>
2020-03-11 14:39:44 -0500

    Consolidate process blocks

--- Part3/Register/register.vhd
+++ Part3/Register/register.vhd
@@ -4,33 +4,74 @@ use IEEE.numeric_std.all;
 
 entity RegisterFile is
     port(
-        rd: in STD_LOGIC_VECTOR(11 downto 8);
-        r1: in STD_LOGIC_VECTOR(7 downto 4);
-        r2: in STD_LOGIC_VECTOR(3 downto 0);
-        cRegWrite: in STD_LOGIC;
+        rd: in STD_LOGIC_VECTOR(11 downto 8) := "0000";
+        r1: in STD_LOGIC_VECTOR(7 downto 4) := "0000";
+        r2: in STD_LOGIC_VECTOR(3 downto 0) := "0000";
+        cRegWrite: in STD_LOGIC := '0';
         input: in STD_LOGIC_VECTOR(15 downto 0);
         inr: in STD_LOGIC_VECTOR(3 downto 0); -- Debugging
-        toOffset: out STD_LOGIC_VECTOR(15 downto 0);
-        toALU: out STD_LOGIC_VECTOR(15 downto 0);
-        toMemory: out STD_LOGIC_VECTOR(15 downto 0);
-        outvalue: out STD_LOGIC_VECTOR(15 downto 0) -- Debugging
+        outr1toOffsetMux: out STD_LOGIC_VECTOR(15 downto 0);
+        outr2toALU: out STD_LOGIC_VECTOR(15 downto 0);
+        toMemory: out STD_LOGIC_VECTOR(15 downto 0) := "0000000000000000";
+        outvalue: out STD_LOGIC_VECTOR(15 downto 0); -- Debugging
+        reset: in STD_LOGIC := '1'
     );
 end RegisterFile;
 
 architecture Behavioral of RegisterFile is
     subtype word is STD_LOGIC_VECTOR(15 downto 0); -- 16-bit register
     type memory is array (0 to 15) of word; -- 16 total registers
-    signal register16: memory;
+    signal register16: memory := (others => "0000000000000000");
         
 begin
+    -- Asynchronous reset 
+    process(reset, rd, r1, r2)
+        begin
+            if (reset = '1') then
+--                register16 <= (0 => "0000000000000000",
+--                              1 => "0000000000000001",
+ --                             2 => "0000000000000011",
+  --                            3 => "0000000000000111",
+   --                           4 => "0000000000001111",
+    --                          5 => "0000000000011111",
+     --                         6 => "0000000000111111",
+      --                        7 => "0000000001111111",
+       --                       8 => "0000000011111111",
+        --                      9 => "0000000111111111",
+         --                     others => "1000000000000000");
+                register16 <= (others => "0000000000000000");
+                outr1toOffsetMux <= "0000000000000000";
+                outr2toALU <= "0000000000000000";
+                toMemory <= "0000000000000000";
+                outvalue <= "0000000000000000";
+                assert register16(0) = "0000000000000000";
+                report "reset on";
+            else
+                outr1toOffsetMux <= register16(to_integer(unsigned(r1)));
+                outr2toALU <= register16(to_integer(unsigned(r2)));
+                toMemory <= register16(to_integer(unsigned(r1))); -- Load or store
+                report "outputs set";
+                outvalue <= "0000000000000000";
+                report "reset off";
+            end if;
+    end process;
+
     -- Write input to register16(r2)
     process (cRegWrite)
         begin
             if (cRegWrite = '1') then
-                register16(to_integer(unsigned(r2))) <= input;
+                -- register16(4) <= "1010101010101010";
+                assert r2 = "0110";
+                assert to_integer(unsigned(r2)) = 6;
+                assert input = "0000000000000000";
+                -- register16(to_integer(unsigned(r2))) <= input;
+                register16(6) <= "1000000000000000";
+                assert register16(6) = "0000000000000000";
+                report "cRegWrite set";
             end if;
     end process;
 
 
 end Behavioral;
+
 
--- Part3/Register/register_tb.vhd
+++ Part3/Register/register_tb.vhd
@@ -1,5 +1,6 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
 entity t_registerfile is
 end t_registerfile;
@@ -13,23 +14,25 @@ architecture behavior of t_registerfile is
             cRegWrite: in STD_LOGIC;
             input: in STD_LOGIC_VECTOR(15 downto 0);
             inr: in STD_LOGIC_VECTOR(3 downto 0); -- Debugging
-            toOffset: out STD_LOGIC_VECTOR(15 downto 0);
-            toALU: out STD_LOGIC_VECTOR(15 downto 0);
+            outr1toOffsetMux: out STD_LOGIC_VECTOR(15 downto 0);
+            outr2toALU: out STD_LOGIC_VECTOR(15 downto 0);
             toMemory: out STD_LOGIC_VECTOR(15 downto 0);
-            outvalue: out STD_LOGIC_VECTOR(15 downto 0) -- Debugging
+            outvalue: out STD_LOGIC_VECTOR(15 downto 0); -- Debugging
+            reset: in STD_LOGIC
         );
     end component RegisterFile;
 
-    signal rd_t: STD_LOGIC_VECTOR(11 downto 8);
-    signal r1_t: STD_LOGIC_VECTOR(7 downto 4);
-    signal r2_t: STD_LOGIC_VECTOR(3 downto 0);
-    signal cRegWrite_t: STD_LOGIC;
-    signal input_t: STD_LOGIC_VECTOR(15 downto 0);
-    signal inr_t: STD_LOGIC_VECTOR(3 downto 0); -- Debugging
-    signal toOffset_t: STD_LOGIC_VECTOR(15 downto 0);
-    signal toALU_t: STD_LOGIC_VECTOR(15 downto 0);
-    signal toMemory_t: STD_LOGIC_VECTOR(15 downto 0);
-    signal outvalue_t: STD_LOGIC_VECTOR(15 downto 0); -- Debugging
+    signal rd_t: STD_LOGIC_VECTOR(11 downto 8) := "0000";
+    signal r1_t: STD_LOGIC_VECTOR(7 downto 4) := "0000";
+    signal r2_t: STD_LOGIC_VECTOR(3 downto 0) := "0000";
+    signal cRegWrite_t: STD_LOGIC := '0';
+    signal input_t: STD_LOGIC_VECTOR(15 downto 0) := "0000000000000000";
+    signal inr_t: STD_LOGIC_VECTOR(3 downto 0) := "0000"; -- Debugging
+    signal outr1toOffsetMux_t: STD_LOGIC_VECTOR(15 downto 0) := "0000000000000000";
+    signal outr2toALU_t: STD_LOGIC_VECTOR(15 downto 0) := "0000000000000000";
+    signal toMemory_t: STD_LOGIC_VECTOR(15 downto 0) := "0000000000000000";
+    signal outvalue_t: STD_LOGIC_VECTOR(15 downto 0) := "0000000000000000"; -- Debugging
+    signal reset_t: STD_LOGIC := '1';
 
     begin
         rf: RegisterFile
@@ -40,20 +43,30 @@ architecture behavior of t_registerfile is
             cRegWrite => cRegWrite_t,
             input => input_t,
             inr => inr_t,
-            toOffset => toOffset_t,
-            toALU => toALU_t,
+            outr1toOffsetMux => outr1toOffsetMux_t,
+            outr2toALU => outr2toALU_t,
             toMemory => toMemory_t,
-            outvalue => outvalue_t
+            outvalue => outvalue_t,
+            reset => reset_t
         );
         process
             begin
+                reset_t <= '1';
+                wait for 1 ms;
+                reset_t <= '0';
                 -- Test writing to a register
                 rd_t <= "0100"; -- register 4
                 r1_t <= "0101"; -- register 5
                 r2_t <= "0110"; -- register 6
-                register16(6) <= "0000000000001110";
+                wait for 1 ms;
                 cRegWrite_t <= '1';
-                assert register16(4) = "0000000000001110";
+                wait for 1 ms;
+                -- assert register16(4) = "0000000000001110";
+                -- report to_hex_string(outr1toOffsetMux_t);
+                -- assert outr1toOffsetMux_t = "0000000000011111"; -- Reg 5
+                -- assert outr2toALU_t = "0000000000111111"; -- Reg 6
+                -- assert cRegWrite_t = '1';
+                wait for 10000 ms;
         end process;
 
 end behavior;
